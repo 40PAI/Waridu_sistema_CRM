cf6bde62cccf0c19819431061d6ff780b15d2c15
[dyad] <edfrancisco1718@gmail.com>
2025-09-11 17:07:32 +0100

    [dyad] Updated BrowserRouter to include v7 future flags to silence React Router warnings - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ function App() {
   return (
     <ErrorBoundary>
       <AuthProvider>
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route element={<ProtectedRoute />}>
