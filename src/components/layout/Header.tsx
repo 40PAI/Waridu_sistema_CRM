7cf11179b8505dd71093af07a646ec43c3bdb263
[dyad] <edfrancisco1718@gmail.com>
2025-09-18 10:38:10 +0100

    [dyad] Added missing Header component - wrote 1 file(s)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,1 +1,15 @@
 import { Menu, Users, FileText, Settings, Home, Users2, Package, CalendarDays, Bell, KanbanSquare, CheckCircle, LayoutGrid, Calendar } from "lucide-react";
+import NotificationsBell from "@/components/notifications/NotificationsBell";
+
+const Header = () => {
+  return (
+    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+      <div className="w-full flex-1">
+        {/* Header content can be added here */}
+      </div>
+      <NotificationsBell />
+    </header>
+  );
+};
+
+export default Header;
