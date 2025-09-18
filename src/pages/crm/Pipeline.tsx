639fd25632d74df8e70ccb0f10bc32d023f64176
[dyad] <edfrancisco1718@gmail.com>
2025-09-18 08:37:22 +0100

    Reverted all changes back to version 523cd748d7a90f7a0702f2c89f8be27387e23b90

--- src/pages/crm/Pipeline.tsx
+++ src/pages/crm/Pipeline.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import ProjectsPage from "./Projects";
-
-export default function PipelinePage(): JSX.Element {
-  return <ProjectsPage />;
+export default function PipelinePage() {
+  // ... existing code
 }
